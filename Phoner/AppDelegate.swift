e2fd46bdd0845a96d5b986cfe927874310377447
omardlhz <omar.dlhz@hotmail.com>
2016-01-09 22:56:18 -0500

    Initial Commit

---
+++ Phoner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Phoner
+//
+//  Created by Omar Dlhz on 1/9/16.
+//  Copyright © 2016 Omar De La Hoz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Phoner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Phoner
+//
+//  Created by Omar Dlhz on 1/9/16.
+//  Copyright © 2016 Omar De La Hoz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
