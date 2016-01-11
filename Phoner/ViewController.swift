3b8c83ec6bc8a3086c71e1f7d57a28863bfb2563
omardlhz <omar.dlhz@hotmail.com>
2016-01-11 01:01:15 -0500

    Initial commit

--- Phoner/ViewController.swift
+++ Phoner/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import Cocoa
+import Foundation
+import MultipeerConnectivity
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, MCSessionDelegate {
+    
+    var peerID: MCPeerID!
+    var mcSession: MCSession!
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        peerID = MCPeerID(displayName: "Server")
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .Required)
+        mcSession.delegate = self
+        
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-kb", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant.start()
 
         // Do any additional setup after loading the view.
     }
@@ -20,6 +33,32 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    
+    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+        return
+    }
+    
+    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {
+        
+        return
+    }
+    
+    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
+        
+        return
+    }
+    
+    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        
+        return
+    }
+    
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+        
+        return
     }
 
 
